b3082a101c557e26f71720bc62572b8d3d3a1050
Ayesha sidiqa <113618345+Sidiqayesha@users.noreply.github.com>
2024-07-31 10:22:27 +0530

    scheduler example with email sending

---
+++ src/test/java/com/example/schedulerExample/SchedulerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulerExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
